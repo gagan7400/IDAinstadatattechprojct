9e4457c88a1625ff555dbe1af312995d887edc12
Gagan <vishwakarmagagan24@gmail.com>
2023-11-02 11:14:06 +0530

    Update App.js

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -1,4 +1,7 @@
-
+import Headernav from './Components/Headernav'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
 import './App.css';
 
 function App() {
