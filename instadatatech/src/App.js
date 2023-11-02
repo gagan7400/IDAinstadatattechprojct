dfbce159db1e97c997781240705ba21370cfc5b6
Raviii02 <137808623+Raviii02@users.noreply.github.com>
2023-11-02 12:46:07 +0530

    update in appjs
    update

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -1,20 +1,20 @@
-import Headernav from './Components/Headernav'
-import Navbar from './Components/Navbar'
-import Home from './Components/Home'
-import Footer from './Components/Footer'
+import Headernav from './components/Headernav'
+import Navbar from './components/Navbar'
+import Home from '.components/Home'
+import Footer from './components/Footer'
 import './App.css';
-import Blog from './Components/blog/Blog';
+import Blog from './components/blog/Blog';
 
 function App() {
   return (
     <>
-    <Blog/>
-    <Headernav/>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    <Contact/>
-      </>
+      <Blog />
+      <Headernav />
+      <Navbar />
+      <Home />
+      <Footer />
+      <Contact />
+    </>
   );
 }
 
