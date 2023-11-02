bc623f6063eb010f29a5657219beb29d93bf688a
Gagan <vishwakarmagagan24@gmail.com>
2023-11-02 11:52:35 +0530

    update components name

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -1,7 +1,7 @@
-import Headernav from './components/Headernav'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
+import Headernav from './Components/Headernav'
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
 import './App.css';
 import Blog from './Components/blog/Blog';
 
