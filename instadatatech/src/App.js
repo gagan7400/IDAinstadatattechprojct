d4c92c8e10106d00ed8afaef08149a0a2977831f
Gagan <vishwakarmagagan24@gmail.com>
2023-11-02 12:13:43 +0530

    .
    .

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -2,6 +2,7 @@ import Headernav from './Components/Headernav'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
+import Contact from './Components/Contact'
 import './App.css';
 import Blog from './Components/blog/Blog';
 import Services from './Components/Services';
@@ -13,11 +14,10 @@ function App() {
     <Navbar/>
     <Home/>
     <Blog/>
-       <Contact/>
-    <Footer/>
- 
     <Contact/>
     <Services/>
+      <Footer/>
+ 
          
      </>
   );
