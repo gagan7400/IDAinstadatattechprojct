d6f3cfd8d0a45262cea58f5908a3160953aeefc2
namanthakurr <thakurnaman07@gmail.com>
2023-11-02 12:05:27 +0530

    Service Component
    Service imported , Service Css

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -1,20 +1,9 @@
-<<<<<<< Updated upstream
 import Headernav from './components/Headernav'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import './App.css';
 import Blog from './Components/blog/Blog';
-=======
-import Headernav from './Components/Headernav'
-import Navbar from './Components/Navbar'
-
-import Home from './Components/Home'
-import Footer from './Components/Footer'
-import Contact from './Components/Contact'
-import './App.css';
-import Services from './Components/Services';
->>>>>>> Stashed changes
 
 function App() {
   return (
