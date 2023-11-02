ee35fae7b957b0a9955444e7301c3ad8e880cc7e
namanthakurr <thakurnaman07@gmail.com>
2023-11-02 11:48:12 +0530

    Services imported
    service component , added a link in intex.html

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -1,9 +1,19 @@
+<<<<<<< Updated upstream
 import Headernav from './components/Headernav'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import './App.css';
 import Blog from './Components/blog/Blog';
+=======
+import Headernav from './Components/Headernav'
+import Navbar from './Components/Navbar'
+
+import Home from './Components/Home'
+import Footer from './Components/Footer'
+import './App.css';
+import Services from './Components/Services';
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -13,8 +23,12 @@ function App() {
     <Navbar/>
     <Home/>
     <Footer/>
+<<<<<<< Updated upstream
     <Contact/>
          
+=======
+    <Services/>
+>>>>>>> Stashed changes
      </>
   );
 }
