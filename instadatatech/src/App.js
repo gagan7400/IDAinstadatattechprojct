d0c3b1a877aca56d666912f6e32f8fa380122a49
VinaySingh7112 <vinaysinghraksel11@gmail.com>
2023-11-03 11:34:17 +0530

    app.js
    app

--- instadatatech/src/App.js
+++ instadatatech/src/App.js
@@ -5,28 +5,35 @@ import Footer from './Components/Footer'
 import Contact from './Components/Contact'
 import './App.css';
 import Blog from './Components/blog/Blog';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-import '../node_modules/bootstrap/dist/js/bootstrap.js.map'
-import Services from "./Components/Services.jsx"
-import About from './Components/About'
+import Services from './Components/Services';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Testimonials from './Components/Testimonials'
 import Swiper_footer from './Components/Swiper_footer'
 
 function App() {
   return (
-    <>
-    <Headernav/>
-    <Navbar/>
-    <Home/>
-    <Blog/>
-    <Contact/>
-    <Services/>
-      <Footer/>
- 
-         
-     </>
+    <BrowserRouter>
+      <Headernav/>
+      <Navbar/>
+      <Routes>
+
+        <Route path='/'element={<Home />} />
+        {/* <Route path='/products' element={<Products/>} /> */}
+        <Route path='/blog' element={<><Blog /><Services /></>} />
+        <Route path='/services' element={<Services />} />
+        {/* <Route path='/about' element={<About/>} /> */}
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
+
+      <Testimonials/>
+      <Swiper_footer/>
+
+      <Footer />
+
+
+    </BrowserRouter>
   );
 }
 
